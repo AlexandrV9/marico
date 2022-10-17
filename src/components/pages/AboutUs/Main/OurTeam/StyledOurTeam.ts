67b2fd145202b81c7e65c0c1150cd344c0ceadd6
Alexandr <vashchekin01@mail.ru>
2022-10-17 20:25:47 +0300

    refac: editing styles for h2 in OurTeam component

--- src/components/pages/AboutUs/Main/OurTeam/StyledOurTeam.ts
+++ src/components/pages/AboutUs/Main/OurTeam/StyledOurTeam.ts
@@ -3,6 +3,16 @@ import styled from "styled-components";
 export default styled.section`
   margin-bottom: 19.5rem;
 
+  h2 {
+    font-weight: 600;
+    font-size: 75px;
+    line-height: 112px;
+    text-align: center;
+    max-width: 93.8rem;
+    margin: 0 auto;
+    margin-bottom: 3.3rem;
+  }
+
   .subtitle {
     text-align: center;
     font-weight: 600;
